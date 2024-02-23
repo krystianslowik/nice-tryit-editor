449534b12ed6595bb152c1480bdaccc7c7600146
krystianslovik <krystian.slowik@trustedshops.com>
2024-02-23 23:47:50 +0100

    Initial commit - setup necessary files
    Setup all mandatory files + setup some issues

---
+++ src/components/EditorArea.js
@@ -0,0 +1,89 @@
+import React, { useState, useRef } from "react";
+import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
+
+import MonacoEditor from "react-monaco-editor";
+import "monaco-editor/esm/vs/basic-languages/html/html"; // Import HTML language support
+
+import Navbar from "./Navbar";
+import OutputWindow from "./OutputWindow";
+
+const EditorArea = () => {
+  const [code, setCode] = useState(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+  </head>
+  <body>
+      
+
+
+  </body>
+  </html>`);
+  const [outputHtml, setOutputHtml] = useState("");
+  const editorRef = useRef(null);
+  const monacoInstanceRef = useRef(null);
+
+  const handleEditorChange = (newValue) => {
+    setCode(newValue);
+  };
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+    monacoInstanceRef.current = monaco;
+  };
+
+  const handleSnippetInsert = (snippet) => {
+    if (!editorRef.current || !monacoInstanceRef.current) return;
+
+    const position = editorRef.current.getPosition();
+    const range = new monacoInstanceRef.current.Range(
+      position.lineNumber,
+      position.column,
+      position.lineNumber,
+      position.column
+    );
+    editorRef.current.executeEdits("", [{ range: range, text: snippet }]);
+    editorRef.current.focus();
+  };
+
+  const handleUpdateOutput = () => {
+    setOutputHtml(code);
+  };
+  return (
+    <div className="flex w-full h-screen">
+      <div className="flex flex-col w-1/2 h-full">
+        <Navbar onSnippetInsert={handleSnippetInsert} />
+        <MonacoEditor
+          width="calc(100%)"
+          height="calc(100% - 64px)"
+          language="HTML"
+          theme="vs-dark"
+          value={code}
+          onChange={handleEditorChange}
+          editorDidMount={handleEditorDidMount}
+          options={{
+            minimap: { enabled: true },
+            scrollbar: {
+              verticalScrollbarSize: 0,
+              horizontalScrollbarSize: 0,
+            },
+          }}
+        />
+      </div>
+      <button
+        onClick={handleUpdateOutput}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-4 px-6 rounded-full flex items-center justify-center"
+        title="Update Output"
+      >
+        <ArrowRightCircleIcon className="h-8 w-8" />
+      </button>
+      <div className="w-1/2 h-full" style={{ backgroundColor: "#1e1e1e" }}>
+        <OutputWindow htmlCode={outputHtml} />
+      </div>
+    </div>
+  );
+};
+
+export default EditorArea;
---
+++ src/components/Navbar.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+
+const Navbar = ({ onSnippetInsert }) => {
+  const [selectedSnippet, setSelectedSnippet] = useState("");
+
+  const snippets = [
+    {
+      key: "Trustbadge 5",
+      value: `<script async
+    data-desktop-y-offset="0"
+    data-mobile-y-offset="0"
+    data-desktop-disable-reviews="false"
+    data-desktop-enable-custom="false"
+    data-desktop-position="right"
+    data-desktop-custom-opening-direction="topright"
+    data-desktop-custom-width="150"
+    data-desktop-enable-fadeout="false"
+    data-disable-mobile="false"
+    data-disable-trustbadge="false"
+    data-mobile-custom-width="150"
+    data-mobile-custom-opening-direction="topright"
+    data-mobile-disable-reviews="false"
+    data-mobile-enable-custom="false"
+    data-mobile-position="left"
+    data-mobile-enable-topbar="false"
+    data-color-scheme="light"
+    charset="UTF-8"
+    src="//widgets.trustedshops.com/js/XA2A8D35838AF5F63E5EB0E05847B1CB8.js">
+  </script>`,
+    },
+    {
+      key: "Trustbadge 4",
+      value: `<script type="text/javascript">
+    (function () {
+      var _tsid = 'XA2A8D35838AF5F63E5EB0E05847B1CB8';
+      _tsConfig = {
+        'yOffset': '0', /* offset from page bottom */
+        'variant': 'reviews', /* default, reviews, custom, custom_reviews */
+        'customElementId': '', /* required for variants custom and custom_reviews */
+        'trustcardDirection': '', /* for custom variants: topRight, topLeft, bottomRight, bottomLeft */
+        'customBadgeWidth': '', /* for custom variants: 40 - 90 (in pixels) */
+        'customBadgeHeight': '', /* for custom variants: 40 - 90 (in pixels) */
+        'disableResponsive': 'false', /* deactivate responsive behaviour */
+        'disableTrustbadge': 'false' /* deactivate trustbadge */
+      };
+      var _ts = document.createElement('script');
+      _ts.type = 'text/javascript';
+      _ts.charset = 'utf-8';
+      _ts.async = true;
+      _ts.src = '//widgets.trustedshops.com/js/' + _tsid + '.js';
+      var __ts = document.getElementsByTagName('script')[0];
+      __ts.parentNode.insertBefore(_ts, __ts);
+    })();
+  </script>`,
+    },
+    {
+      key: "Checkout SR",
+      value: `<div id="trustedShopsCheckout" style="display: none;">
+    <span id="tsCheckoutOrderNr">order_variable</span>
+    <span id="tsCheckoutBuyerEmail">cs-testorder@trustedshops.com</span>
+    <span id="tsCheckoutOrderAmount">1</span>
+    <span id="tsCheckoutOrderCurrency">EUR</span>
+    <span id="tsCheckoutOrderPaymentType">COD</span>
+    <span id="tsCheckoutOrderEstDeliveryDate">10.12.2023</span>
+    </div>`,
+    },
+    {
+      key: "Checkout PR",
+      value: `<div id="trustedShopsCheckout" style="display: none;">
+    <span id="tsCheckoutOrderNr">1234_4321</span>
+    <span id="tsCheckoutBuyerEmail">cs-testorder@trustedshops.com</span>
+    <span id="tsCheckoutOrderAmount">amount_variable</span>
+    <span id="tsCheckoutOrderCurrency">currency_variable</span>
+    <span id="tsCheckoutOrderPaymentType">paymentMethod_variable</span>
+    <span id="tsCheckoutOrderEstDeliveryDate">10.12.2023</span>
+    
+   <!-- product reviews start -->
+    <!-- for each product in the basket, full set of data is required -->
+    <span class="tsCheckoutProductItem">
+    <span class="tsCheckoutProductUrl">https://trustedshops.com</span>
+    <span class="tsCheckoutProductImageUrl">https://trustedshops.com</span>
+    <span class="tsCheckoutProductName">Some Product Name</span>
+    <span class="tsCheckoutProductSKU">1234</span>
+    <span class="tsCheckoutProductGTIN">333333333333</span>
+    <span class="tsCheckoutProductMPN">SomeMPN</span>
+    <span class="tsCheckoutProductBrand">SomeBRAND</span>
+    </span>
+    <!-- product reviews end -->
+    </div>`,
+    },
+  ];
+
+  const handleSnippetChange = (e) => {
+    setSelectedSnippet(e.target.value);
+  };
+
+  const handleInsertClick = () => {
+    onSnippetInsert(selectedSnippet);
+  };
+
+  return (
+    <div
+      className="flex justify-center items-cente text-white py-4"
+      style={{ backgroundColor: "#1e1e1e" }}
+    >
+      <select
+        className="bg-gray-700 p-2 rounded mr-4"
+        value={selectedSnippet}
+        onChange={handleSnippetChange}
+      >
+        <option value="">Select a snippet</option>
+        {snippets.map((snippet) => (
+          <option key={snippet.key} value={snippet.value}>
+            {snippet.key}
+          </option>
+        ))}
+      </select>
+      <button
+        className="bg-yellow-600 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleInsertClick}
+      >
+        Insert Snippet
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
